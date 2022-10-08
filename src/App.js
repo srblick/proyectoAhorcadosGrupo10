b2372cd1074875b045411781437cdb44e50c3345
Nayla Aixa Leiva <naylaaixaleiva@gmail.com>
2022-10-08 02:31:20 -0300

    Agregue el componente PageError: Naila

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {MenuInicio} from "./components/MenuInicio"
+import {MenuInicio} from "./components/MenuInicio";
+import PageError from "./components/PageError";
 
 function App() {
     return(
         <BrowserRouter>
             <Routes>
             <Route path="/" element={<MenuInicio/>} />
+            <Route path="*" element={<PageError/>} />
             </Routes>
         </BrowserRouter>
     );
